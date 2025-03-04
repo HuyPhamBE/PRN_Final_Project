62a86bb2894ce0cb059349a3b311929218739fb8
DucHuy <huypd200604@gmail.com>
2025-03-04 08:10:13 +0700

    Interface+Service

---
+++ PRN_Final_Project/Repositories/DB/UOW/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.IUOW;
+using Microsoft.EntityFrameworkCore;
+using Repositories.DB;
+
+namespace Repositories.DB;
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        internal ApplicationDbContext _context;
+        internal DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+            this._dbSet = context.Set<T>();
+        }
+        public IQueryable<T> Entities => _context.Set<T>();
+
+        public async Task DeleteAsync(object id)
+        {
+            T entity = await _dbSet.FindAsync(id) ?? throw new Exception();
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<T?> FirstorDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().FirstOrDefaultAsync(predicate);
+        }
+
+        public async Task<IList<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            return Task.FromResult(_dbSet.Update(entity));
+        }
+    }
+
+}
---
+++ PRN_Final_Project/Repositories/Entities/IUOW/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.IUOW
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        //query
+        IQueryable<T> Entities { get; }
+        //async methods
+
+        Task<IList<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(object id);
+        Task InsertAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(object id);
+        Task SaveAsync();
+        Task<T?> FirstorDefaultAsync(Expression<Func<T, bool>> predicate);
+    }
+}
